d54acba72d5766975bf7f3458fb7f1d7bd6f841c
Paulina Faltyn <faltynp@gmail.com>
2022-12-20 16:18:37 +0100

    Dodano tabele wartosci inflacji

---
+++ projekt_1.py
@@ -0,0 +1,46 @@
+# tabela wartosci inflacji:
+Styczeń = 1.592824484
+Luty = -0.453509101
+Marzec = 2.324671717
+Kwiecień = 1.261254407
+Maj = 1.782526286
+Czerwiec = 2.329384541
+Lipiec = 1.502229842
+Sierpnień = 1.782526286
+Wrzesień = 2.328848994
+Październik = 0.616921348
+Listopad = 2.352295886
+Grudzień = 0.337779545
+Styczeń = 1.577035247
+Luty = -0.292781443
+Marzec = 2.48619659
+Kwiecień = 0.267110318
+Maj = 1.417952672
+Czerwiec = 1.054243267
+Lipiec = 1.480520104
+Sierpnień = 1.577035247
+Wrzesień = -0.07742069
+Październik = 1.165733399
+Listopad = -0.404186718
+Grudzień = 1.499708521
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
